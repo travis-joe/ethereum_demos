491b45411a52e63190b9ea048500478de1c3dfe6
travis joe <weelgod@gmail.com>
2018-05-19 19:01:47 +0800

    56

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -5,6 +5,8 @@ const provider = ganache.provider();
 const web3 = new Web3(provider);
 const { interface, bytecode } = require("../compile.js");
 
+
+const INITIAL_STRING = "Hi there!";
 let accounts;
 let inbox;
 beforeEach(async () => {
@@ -13,7 +15,7 @@ beforeEach(async () => {
   //use one of accounts to deploy
   //the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
     .send({ from: accounts[0], gas: "1000000" });
   inbox.setProvider(provider);
 });
@@ -22,8 +24,15 @@ describe("Inbox", () => {
   it("deploys a contract", () => {
     assert.ok(inbox.options.address);
   });
+
   it("has a default message", async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, "Hi there!");
+    assert.strictEqual(message, INITIAL_STRING);
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, 'bye');
   });
 });
