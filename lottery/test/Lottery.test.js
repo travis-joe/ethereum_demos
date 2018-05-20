4d4058daad9c45e103f070b2d7386b0ca342b752
travis joe <weelgod@gmail.com>
2018-05-21 06:55:54 +0800

    84

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -56,11 +56,27 @@ describe("Lottery Contract", () => {
   it("only manager can call pickWinner", async () => {
     try {
       await lottery.methods.pickWinner().send({
-        from: accounts[1],
+        from: accounts[1]
       });
       assert(false);
     } catch (e) {
       assert(e);
     }
+  });
+
+  it("send money to winner and reset players", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+
+    assert(difference > web3.utils.toWei("0.018", "ether"));
+
   });
 });
