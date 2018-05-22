27e93fe7827d5c3c2ceff0e1eb4c535197472314
travis joe <weelgod@gmail.com>
2018-05-22 08:21:40 +0800

    91

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,68 @@
+import web3 from "./web3";
+
+const address = "0x24c381B573E96c5a3505410f9771d7796B5501F3";
+
+const abi = [
+  {
+    constant: false,
+    inputs: [],
+    name: "returnEntries",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
