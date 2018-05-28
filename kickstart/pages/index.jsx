64408d72b254a8a47a212336789fc34f25c43008
travis joe <weelgod@gmail.com>
2018-05-28 20:04:51 +0800

    160

--- kickstart/pages/index.jsx
+++ kickstart/pages/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
+import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -7,13 +9,29 @@ class CampaignIndex extends Component {
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      };
+    });
+    return <Card.Group items={items} />;
+  }
   render() {
     return (
-      <div>
-        {this.props.campaigns.map(campaign => (
-          <div key={campaign}>{campaign}</div>
-        ))}
-      </div>
+      <Layout>
+        <div>
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+          />
+          <h3>Open Campaigns</h3>
+          {this.renderCampaigns()}
+          <Button content="Create Campaign" icon="add circle" primary />
+        </div>
+      </Layout>
     );
   }
 }
