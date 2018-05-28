7c15ad7d29a17f9061adaa7dc60fc4b04e2ef8c5
travis joe <weelgod@gmail.com>
2018-05-29 06:08:45 +0800

    168

---
+++ kickstart/pages/campaigns/new.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layout";
+import { Form, Button, Input } from "semantic-ui-react";
+import factory from "../../ethereum/factory";
+import web3 from '../../ethereum/web3'
+
+class CampaignNew extends Component {
+  state = {
+    minimumContribution: ""
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    await factory.methods.createCampaign(this.state.minimumContribution).send({
+      from: accounts[0]
+    })
+  };
+
+  render() {
+    return (
+      <Layout>
+        <h3>Create a Campaign</h3>
+
+        <Form onSubmit={this.onSubmit}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={e =>
+                this.setState({ minimumContribution: e.target.value })
+              }
+            />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </Form>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignNew;
