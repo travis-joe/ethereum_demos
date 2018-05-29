bce07f6a5822bee7c897117e13f54531bb02aaa5
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-05-29 17:47:50 +0800

    201

--- kickstart/pages/campaigns/requests/index.jsx
+++ kickstart/pages/campaigns/requests/index.jsx
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import { Form, Button, Input, Message } from "semantic-ui-react";
 import Layout from "../../../components/Layout";
 import { Link, Router } from "../../../routes";
+import Campaign from "../../../ethereum/campaign";
+import web3 from "../../../ethereum/web3";
+
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
-    return { address };
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill(0)
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+    console.log(requests);
+    return { address, requests, requestCount };
   }
 
   render() {
