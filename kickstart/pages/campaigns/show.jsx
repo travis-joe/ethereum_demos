fa9345388d9b100ec06a095c25eb11cb9ef25c85
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-05-29 10:56:41 +0800

    182

--- kickstart/pages/campaigns/show.jsx
+++ kickstart/pages/campaigns/show.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from "react";
 import Layout from "../../components/Layout";
 import {Form, Button, Input, Message} from "semantic-ui-react";
-import factory from "../../ethereum/factory";
-import web3 from "../../ethereum/web3";
+import Campaign from "../../ethereum/campaign";
 import {Link, Router} from '../../routes'
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const {address} = props.query;
+    const summary = await Campaign(address).methods.getSummary().call();
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
+  }
+
   render() {
     return (
       <Layout>
