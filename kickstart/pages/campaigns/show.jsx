c12cacb8951f32391b16c20a6b71b4bff8e502ce
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-05-29 12:05:51 +0800

    184

--- kickstart/pages/campaigns/show.jsx
+++ kickstart/pages/campaigns/show.jsx
@@ -1,9 +1,9 @@
 import React, {Component} from "react";
 import Layout from "../../components/Layout";
-import {Form, Button, Input, Message} from "semantic-ui-react";
+import {Card, Form, Button, Input, Message} from "semantic-ui-react";
 import Campaign from "../../ethereum/campaign";
 import {Link, Router} from '../../routes'
-
+import web3 from '../../ethereum/web3'
 class CampaignShow extends Component {
   static async getInitialProps(props) {
     const {address} = props.query;
@@ -17,10 +17,45 @@ class CampaignShow extends Component {
     };
   }
 
+  renderCards() {
+    const {minimumContribution, balance, requestsCount, approversCount, manager} = this.props;
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of manager',
+        description: 'who create campaign',
+        style: {overflowWrap: 'break-word'}
+      },
+      {
+        header: minimumContribution,
+        meta: 'minimum contribution(wei)',
+        description: 'you must contribute at least this much wei',
+      },
+      {
+        header: requestsCount,
+        meta: 'number of requests',
+        description: 'a request tries to withdraw money',
+      },
+      {
+        header: approversCount,
+        meta: 'number of approvers',
+        description: 'people who a;ready donated to this',
+      },
+      {
+        header: web3.utils.fromWei(balance,'ether'),
+        meta: 'Campaign balance',
+        description: 'how much money left to spend',
+      },
+    ];
+
+    return <Card.Group items={items}/>
+  }
+
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     )
   }
